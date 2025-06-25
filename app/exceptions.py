6facc651afcc54f4c6d82bc79e328aa4e0b3bffe
jaydrava <jraval.0901@gmail.com>
2025-06-25 19:10:54 -0400

    feat: add custom exception classes for error handling

---
+++ app/exceptions.py
@@ -0,0 +1,18 @@
+class CalculatorError(Exception):
+    """ Base class for all calculator-related exceptions."""
+    pass
+
+
+class OperationsError(CalculatorError):
+    """ Raised for invalid operations or failures in operations execution. """
+    pass
+
+
+class ValidationError(CalculatorError):
+    """ Raised when user input is invalid."""
+    pass
+
+
+class ConfigError(CalculatorError):
+    """ Raised when Configration loading fails."""
+    pass
